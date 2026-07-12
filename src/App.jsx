2d53bed41fdf046d9589dfb9fa4a8e9f2b5793d6
Yash Lodam <yashlodam03@gmail.com>
2026-07-13 00:15:56 +0530

    creating pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ChatHome from './pages/ChatHome'
+import Chat from './pages/Chat'
 function App() {
   return (
-    <div>
-      <Button variant={"default"}>Click Me</Button>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<ChatHome/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        
+        </Routes> 
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/ChatHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatHome() {
+  return (
+    <div>ChatHome</div>
+  )
+}
+
+export default ChatHome
